2dfc9392440e41e9b32f5864fb413273b5787dba
Mydjer <djerem02@gmail.com>
2016-04-02 13:07:36 +0200

    Ajout link script bootstrap materialize

---
+++ src/main/java/m1/projet3/Board.java
@@ -0,0 +1,11 @@
+package m1.projet3;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+/**
+ * Created by Jérémy on 01/04/2016.
+ */
+public class Board {
+
+}
